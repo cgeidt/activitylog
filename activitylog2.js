cdbba09cf2c9ebbb9baf10c055aa337dc930ef77
cgeidt <cgeidt@web.de>
2015-06-15 09:28:35 +0000

    UploadForm

--- activitylog2.js
+++ activitylog2.js
@@ -1,16 +1,51 @@
 Activities = new Mongo.Collection("activities");
 
-
-if(Meteor.isClient) {
-    Router.route('/', function () {
-        this.layout('layout', {
-            //set a data context for the whole layout
-            data: {
-                activities: Activities.find()
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        UploadServer.init({
+            checkCreateDirectories: true,
+            maxFileSize: 10000000,
+            tmpDir: '/Uploads/tmp',
+            uploadDir: '/Uploads/',
+            checkCreateDirectories: true,
+            finished: function (fileInfo, formFields) {
+                console.log(fileInfo);
+            },
+            cacheTime: 100,
+            mimeTypes: {
+                "xml": "application/xml",
+                "tcx": "application/tcx"
             }
-        });
-
-        // will just get the data context from layout
-        this.render('activitylist');
+        })
     });
 }
+
+Router.route('/', function () {
+    this.layout('layout', {
+        //set a data context for the whole layout
+        data: {
+            activities: Activities.find()
+        }
+    });
+
+    // will just get the data context from layout
+    this.render('activitylist');
+});
+
+Router.route('/new', function () {
+
+    this.layout('layout', {
+        //set a data context for the whole layout
+
+    });
+
+    // will just get the data context from layout
+    this.render('newActivity');
+
+});
+
+
+
+
+
+
